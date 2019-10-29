50a70d03c579549be61b7aab12b89c6c6c3eb0e2
Thomas Gak Deluen <tgdn45@gmail.com>
2019-10-29 16:11:19 +0000

    Removed border radius from blogroll images

--- src/components/PreviewCompatibleImage.js
+++ src/components/PreviewCompatibleImage.js
@@ -3,21 +3,39 @@ import PropTypes from 'prop-types'
 import Img from 'gatsby-image'
 
 const PreviewCompatibleImage = ({ imageInfo }) => {
-  const imageStyle = { borderRadius: '5px' }
-  const { alt = '', childImageSharp, image } = imageInfo
+  const imageStyle = {};
+  const { alt = '', childImageSharp, image } = imageInfo;
+
+  console.log(imageInfo)
 
   if (!!image && !!image.childImageSharp) {
     return (
-      <Img style={imageStyle} fluid={image.childImageSharp.fluid} alt={alt} />
+      <Img
+        style={imageStyle}
+        fluid={image.childImageSharp.fluid}
+        alt={alt}
+      />
     )
   }
 
   if (!!childImageSharp) {
-    return <Img style={imageStyle} fluid={childImageSharp.fluid} alt={alt} />
+    return (
+      <Img
+        style={imageStyle}
+        fluid={childImageSharp.fluid}
+        alt={alt}
+      />
+    );
   }
 
   if (!!image && typeof image === 'string')
-    return <img style={imageStyle} src={image} alt={alt} />
+    return (
+      <img
+        style={imageStyle}
+        src={image}
+        alt={alt}
+      />
+    );
 
   return null
 }
